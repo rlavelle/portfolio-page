c1531b0243f98da212b9c69112080c7070c6da80
rowlavel <rowlavel@iu.edu>
2021-02-15 22:42:55 -0500

    static web files + project page html files

---
+++ static/js/home.js
@@ -0,0 +1,24 @@
+$(function() {
+  /* NOTE: hard-refresh the browser once you've updated this */
+  $(".typed").typed({
+    strings: [
+      "stat rowlavel.human<br/>" + 
+      "><span class='caret'>$</span> skills: Artificial Intelligence, Machine Learning, Statistics, Teaching<br/> ^100" +
+      "><span class='caret'>$</span> job: Teaching Assistant at Indiana University Bloomington</a><br/> ^100"
+    ],
+    showCursor: true,
+    cursorChar: '_',
+    autoInsertCss: true,
+    typeSpeed: 0.001,
+    startDelay: 50,
+    loop: false,
+    showCursor: false,
+    onStart: $('.message form').hide(),
+    onStop: $('.message form').show(),
+    onTypingResumed: $('.message form').hide(),
+    onTypingPaused: $('.message form').show(),
+    onComplete: $('.message form').show(),
+    onStringTyped: function(pos, self) {$('.message form').show();},
+  });
+  $('.message form').hide()
+});
